1ac3bea90a0736c82e687ebf9b021bbabdc559cd
Alex <alex.phazz@gmail.com>
2022-10-12 16:20:39 +0300

    practice

---
+++ basic.rb
@@ -0,0 +1,17 @@
+# while loop
+$i = 0
+$num = 10
+while $i <
+$num  do
+puts("Inside the loop i = #$i" )
+$i = $i + 1
+end
+
+# until Loop 
+
+$i = 10
+$num = 0
+until $i < $num  do
+puts("Inside the loop i = #$i" )
+$i = $i - 1;
+end
